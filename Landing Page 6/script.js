07e24631a1095b79a5a3f38daa75ef432afff1bf
Kaviya <kaviya.arunagiri2478@gmail.com>
2024-12-14 18:26:34 +0530

    add

---
+++ Landing Page 6/script.js
@@ -0,0 +1,18 @@
+gsap.to(".imagecontainer",{
+    ease: Expo.easeInOut,
+    width:"100%",
+    stagger:2
+})
+
+gasp.to(".text h1", {
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:0
+})
+
+gasp.to(".text h1", {
+    delay:2,
+    ease: Expo.easeInOut,
+    stagger:2,
+    top:"-100%"
+})
